1246d60a753a0de2b16cbefa29905bb6d45dc03b
your name <kikanaide09@gmail.com>
2025-05-13 10:26:30 +0700

    chane lap1_p2

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import { registerRootComponent } from 'expo';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import Router from './lap2/Router';
+import Calculator from './lap1_p2/calulator';
 
 const App = () => (
   <SafeAreaProvider>
-    <Router />
+    <Calculator />
   </SafeAreaProvider>
 );
 
